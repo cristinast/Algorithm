ebe1cdfa9a5ef00bf09c30cc4fe933bf04c0a946
cristinast <gg.cristinast@gmail.com>
2018-12-12 11:11:51 +0900

    finish heap

--- AlgorithmSort/main.cpp
+++ AlgorithmSort/main.cpp
@@ -45,11 +45,43 @@ void ShellSort(int list[],int n){
 
 //Heapsort
 
+void ProcDown(int list[],int i,int n);
+void Heapsort(int list[],int n);
+void Swap(int &a,int &b);
+
+void ProcDown(int list[],int i,int n){
+    int child;
+    int temp;
+    for(temp = list[i]; 2*i+1 < n;i = child){
+        child = 2*i+1;
+        if(child != n - 1 && list[child] < list[child+1]) ++child;
+        if(temp < list[child])
+            Swap(list[i],list[child]);
+        else break;
+    }
+
+}
+void Swap(int &a,int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void Heapsort(int list[],int n){
+    int i;
+    for(i = n/2;i >= 0;i--)
+        ProcDown(list,i,n);
+    for(i = n-1;i > 0;i--){
+        Swap(list[0],list[i]);
+        ProcDown(list,0,i);
+    }
+}
+
 
 int main(){
     int Unordered[8]{3,1,4,5,9,2,6,5};
     // InsertionSort(Unordered,8);
     // ShellSort(Unordered,8);
+    Heapsort(Unordered,8);
     for(int i = 0;i < 8;i++)
     cout<<" "<<Unordered[i]<<" ";
     return 0;
